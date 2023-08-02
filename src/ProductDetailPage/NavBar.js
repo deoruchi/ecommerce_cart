1537911dbdde24cb317df8dc63e9d8282c7171f0
kurokurotho <deoruchi064@gmail.com>
2023-08-02 12:01:21 +0530

    deployment

--- src/ProductDetailPage/NavBar.js
+++ src/ProductDetailPage/NavBar.js
@@ -1,26 +1,34 @@
+import Logo from "../images/logo.svg";
+import Ham from "../images/icon-menu.svg";
+import Profile from "../images/image-avatar.png";
+import Cart from "../images/icon-cart.svg";
 export default function NavBar() {
   return (
-    <nav className="">
+    <nav className=" flex flex-col md:flex-row md:justify-between items-center py-3 md:mx-32 border-y-slate-500 border-b-2 ">
       {/* hamburger icon */}
-      <img></img>
+      <img src={Ham} className=" w-6 h-4 md:hidden "></img>
       {/* logo */}
-      <img></img>
+
       {/* navigation items */}
 
-      <div>
-        <ul>
-          <li>Collections</li>
-          <li>Men</li>
-          <li>Women</li>
-          <li>About</li>
-          <li>Contact</li>
+      <div className="md:flex md:flex-row md:items-center">
+        <img src={Logo} className=" w-26 h-5"></img>
+        <ul className="flex md:flex-row md:mx-2 list-none text-slate-500 ">
+          <li className="md:p-2">Collections</li>
+          <li className="md:p-2">Men</li>
+          <li className="md:p-2">Women</li>
+          <li className="md:p-2">About</li>
+          <li className="md:p-2">Contact</li>
         </ul>
       </div>
 
-      <button>Cart</button>
+      {/* <button>Cart</button> */}
       {/* profilte pic */}
-      <div>
-        <img></img>
+      <div className="md:flex md:flex-row md:items-center  ">
+        <button className="px-4">
+          <img src={Cart}></img>
+        </button>
+        <img src={Profile} className="w-12"></img>
       </div>
     </nav>
   );
